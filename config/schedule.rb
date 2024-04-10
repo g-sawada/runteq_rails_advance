e17bb72f8a12c951d8d1b364040343fbfa9b0929
g.sawada <sawada.gota@gmail.com>
2024-04-10 12:37:28 +0900

    wheneverのテスト実行確認

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,6 +7,7 @@
 require File.expand_path(File.dirname(__FILE__) + "/environment") # Rails.root(Railsメソッド)を使用するために必要
 rails_env = ENV['RAILS_ENV'] || :development # cronを実行する環境変数(:development, :product, :test)
 set :environment, rails_env # cronを実行する環境変数をセット
+ENV.each { |k, v| env(k, v) } #環境変数をwheneverのジョブに引き継ぐのに必要らしい
 set :output, "#{Rails.root}/log/crontab.log" # cronのログ出力用ファイル
 
 every 1.minutes do
--- lib/tasks/example_task.rake
+++ lib/tasks/example_task.rake
@@ -1,8 +1,9 @@
 namespace :example do
   desc "This is an example task"
   task task: :environment do
+    now = Time.now
     puts "========================================
-                  Example task executed!
+          Example task executed! at #{now}
           ========================================"
   end
 end
