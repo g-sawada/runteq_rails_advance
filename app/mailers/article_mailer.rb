2a32903187100f08749b4ab315427a6037e4efe5
g.sawada <sawada.gota@gmail.com>
2024-04-21 17:16:23 +0900

    publishe_at_yesterday scopeとメーラーコントローラーの編集

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -1,2 +1,7 @@
 class ArticleMailer < ApplicationMailer
+  def report_summary
+    @published_articles = Article.published
+    @published_yesterday_articles = Article.published_at_yesterday
+    mail(to: 'admin@exmample.com', subject: '公開済記事の集計結果')
+  end
 end
