90f5f22cb4742bf8edf3c4c11b23d55c69ab9c3f
g.sawada <sawada.gota@gmail.com>
2024-04-21 18:03:32 +0900

    specファイルの更新と微修正

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -2,6 +2,6 @@ class ArticleMailer < ApplicationMailer
   def report_summary
     @published_articles = Article.published
     @published_yesterday_articles = Article.published_at_yesterday
-    mail(to: 'admin@exmample.com', subject: '公開済記事の集計結果')
+    mail(to: 'admin@example.com', subject: '公開済記事の集計結果')
   end
 end
--- spec/mailers/previews/article_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/article
-class ArticlePreview < ActionMailer::Preview
-
-end
